8c97aec8f287a7218b7e15a8cf4fb1217846874b
moong94 <hmg19940201@naver.com>
2021-04-29 20:03:52 +0900

    Revise headermenu, contents

--- Yonsei-Koica-Homepage/www/sub/network.php
+++ Yonsei-Koica-Homepage/www/sub/network.php
@@ -8,9 +8,9 @@ include_once('../common.php');
 
 
 
-// 페이지 제목
 
 
+$g5['title'] = "새로운 페이지";
 
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
@@ -27,9 +27,11 @@ include_once(G5_PATH.'/head.php');
 <head>
   <style>
   ul, ol, li{ list-style:none; margin:0; padding:0; }
-
+  .mytable {
+        margin:0px -265px;background: ;text-align: center;background: #86A5D0; border-bottom: 2px solid #123059; color: #163B6E;height: 15px;font-size: 16;background: #fafafa;
+      }
 ul.myMenu {}
-ul.myMenu > li { width:319px; padding:5px 10px; background:#4e6791; font-size: 12pt; border:1px solid #163B6E; color: white; text-align:center; position:relative; margin: 0px -200px; }
+ul.myMenu > li { width:319px; padding:5px 10px; background:#4e6791; font-size: 12pt; border:1px solid #163B6E; color: white; text-align:center; position:relative; margin: 0px -265px; }
 ul.myMenu > li:hover { background:#4e6791; }
 ul.myMenu > li ul.submenu { display:none; position:absolute; top:30px; left:0; }
 ul.myMenu > li:hover ul.submenu { display:block; }
@@ -39,12 +41,35 @@ ul.myMenu > li ul.submenu > li:hover { background:#5981B7; }
 
 </head>
 
-<div id="container">
-<table style="margin:0px -200px;background: ;text-align: center;background: #163B6E;color: #ffff;height: 80px;font-size: 18;" width="342px" border="0">
-    <tbody><tr><td>COMMUNITY</td></tr>
-  </tbody></table>
 
+<table style="margin:0px -265px;background: ;text-align: center;background: #163B6E; border-bottom: 2px solid #123059; color: #ffff;height: 80px;font-size: 16;" width="342px" border="0">
+    <tbody><tr><td><strong>COMMUNITY<strong></td></tr>
+  </tbody></table>
+  <table class="mytable" width="342px" border="0">
+        <tbody><tr><td style = "cursor:pointer;" onClick = " location.href='http://gmegm.cafe24.com/bbs/board.php?bo_table=notice' " onMouseOver = " window.status = 'http://gmegm.cafe24.com/bbs/board.php?bo_table=notice' " onMouseOut = " window.status = '' ">News & Notices</td></tr>
+      </tbody></table>
 </table>
+<table  class="mytable" width="342px" border="0">
+      <tbody><tr><td style = "cursor:pointer;" onClick = " location.href='http://gmegm.cafe24.com/bbs/board.php?bo_table=thesis' " onMouseOver = " window.status = 'http://gmegm.cafe24.com/bbs/board.php?bo_table=thesis' " onMouseOut = " window.status = '' ">Thesis</td></tr>
+    </tbody></table>
+    <table  class="mytable" width="342px" border="0">
+      <tbody><tr><td style = "cursor:pointer;" onClick = " location.href='http://gmegm.cafe24.com/bbs/board.php?bo_table=graduation' " onMouseOver = " window.status = 'http://gmegm.cafe24.com/bbs/board.php?bo_table=graduation' " onMouseOut = " window.status = '' ">Graduation Report</td></tr>
+    </tbody></table>
+    <table  class="mytable" width="342px" border="0">
+      <tbody><tr><td style = "cursor:pointer;" onClick = " location.href='http://gmegm.cafe24.com/bbs/board.php?bo_table=gallery' " onMouseOver = " window.status = 'http://gmegm.cafe24.com/bbs/board.php?bo_table=gallery' " onMouseOut = " window.status = '' ">Gallery</td></tr>
+    </tbody></table>
+
+
+    <!--
+    <table style="margin:30px -265px;" width="342px" border="1">
+        <tbody><tr><td><strong>COMMUNITY<strong></td></tr>
+          <tr><td><strong>COMMUNITY<strong></td></tr>
+            <tr><td><strong>COMMUNITY<strong></td></tr>
+              <tr><td><strong>COMMUNITY<strong></td></tr>
+      </tbody></table>
+
+    -->
+    <div id="container">
 <ul class="myMenu">
 
     <li class="menu2">
@@ -60,15 +85,11 @@ ul.myMenu > li ul.submenu > li:hover { background:#5981B7; }
 </div>
 
 <!--
-<table border="1" style="float: left;margin-right:30px; margin-left:-285px;margin-top:70px; z-index:1" width="500" height="200">
-<tr><td>3</td></tr>
-<tr><td>3</td></tr>
-<tr><td>3</td></tr>
-</table>
+
 -->
 
 <br><br><br><br><br><br><br><br><br>
-<table style="float: left;margin-right:39px; margin-left:224px;margin-top:-202px; z-index:1" width="100%" height="150" border="1">
+<table  width="100%" height="150" border="1">
   <tbody><tr>
 
   <td width="10%"><img src="../theme/company/img/network/20210428_191006.png" width="100%"></td>
@@ -102,7 +123,81 @@ ul.myMenu > li ul.submenu > li:hover { background:#5981B7; }
   </tr>
 
 </table>
+
 <br><br><br><br>
+
+<table  width="100%" height="150" border="1">
+  <tbody><tr>
+
+  <td width="10%"><img src="../theme/company/img/network/20210429_190916.png" width="100%"></td>
+
+
+  <td width="40%"><h1> KYAW SOE NAING</h1> <br>
+    Myanmar<br>
+    Deputy Director<br>
+    Ministry of Transport and Communications, Information Technology and Cyber Security Department<br><br>
+
+
+    Research Interests <br>
+  e-Government Development processes, cyber security and other IT fields
+<br>
+Career plan in 10 Years<br>
+IT professional in the government sector, especially in e-Government and cyber security sector
+</td>
+    <td width="10%"> <img src="../theme/company/img/network/20210429_190932.png" width="100%"></td>
+      <td width="40%"><br><br>
+      <h1>  VILAYPHONE XAYSONGKHAME</h1><br>
+
+        Lao People's Democratic Republic (Lao PDR/Laos)<br><br>
+
+        Technical official in the field of disability development<br>
+        Department of Policy to Devotees, Disabilities, and Elderlies<br>
+        Ministry of Labor and Social Welfare<br><br>
+
+        Research Interests <br>
+        I am interested in ICT or Data Online System in Korea, especially in terms of the Social Welfare online system for persons with disabilities. Thus, my research expects to study:<br>
+
+
+
+      </td>
+  </tr>
+
+</table>
+<br><br><br><br>
+<table  width="100%" height="150" border="1">
+  <tbody><tr>
+
+  <td width="10%"><img src="../theme/company/img/network/20210429_190956.png" width="100%"></td>
+
+
+  <td width="40%"><h1> SINIT LONH</h1> <br>
+    Cambodia<br>
+    Deputy Head of Office of General Affair<br>
+    Ministry of Industry, Science, Technology and Innovation<br><br>
+
+
+    Research InterestsField of e-Government: Citizen trust in e-Government adoption/services, e-Government as a driving force to economic growth, and e-Government in politics.<br>
+    Career Plan in 10 Years<br>
+    Policy decision making in the area of e-Government</td>
+    <td width="10%"> <img src="../theme/company/img/network/20210429_191012.png" width="100%"></td>
+      <td width="40%"><br><br>
+      <h1>  VILAYPHONE XAYSONGKHAME</h1><br>
+
+        Lao People's Democratic Republic (Lao PDR/Laos)<br><br>
+
+        Technical official in the field of disability development<br>
+        Department of Policy to Devotees, Disabilities, and Elderlies<br>
+        Ministry of Labor and Social Welfare<br><br>
+
+        Research Interests <br>
+        I am interested in ICT or Data Online System in Korea, especially in terms of the Social Welfare online system for persons with disabilities. Thus, my research expects to study:<br>
+
+
+
+      </td>
+  </tr>
+
+</table>
 
 
 <?php
